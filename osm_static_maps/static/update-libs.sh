89f13b5ac2d25047b4721891e58370a52cf72774
Nathan Rennie-Waldock <nathan.renniewaldock@gmail.com>
2023-09-20 12:58:23 +0100

    Change mapbox-gl to maplibre-gl to fix blurry maps caused by fractional positioning

--- osm_static_maps/static/update-libs.sh
+++ osm_static_maps/static/update-libs.sh
@@ -2,11 +2,11 @@
 LEAFLET_VERSION=1.9.4
 LEAFLET_FILES="leaflet.min.css leaflet.min.js images/layers.png images/layers-2x.png images/marker-icon.png images/marker-icon-2x.png images/marker-shadow.png"
 
-MAPBOX_GL_VERSION=2.15.0
-MAPBOX_GL_FILES="mapbox-gl.js mapbox-gl.css"
+MAPLIBRE_GL_VERSION=3.3.1
+MAPLIBRE_GL_FILES="maplibre-gl.js maplibre-gl.css"
 
-MAPBOX_GL_LEAFLET_VERSION=0.0.16
-MAPBOX_GL_LEAFLET_FILES="leaflet-mapbox-gl.min.js"
+MAPLIBRE_GL_LEAFLET_VERSION=0.0.20
+MAPLIBRE_GL_LEAFLET_FILES="leaflet-maplibre-gl.js"
 
 LEAFLET_POLYLINE_VERSION=1.6.0
 LEAFLET_POLYLINE_FILES="leaflet.polylineDecorator.js"
@@ -15,12 +15,12 @@ for file in ${LEAFLET_FILES}; do
 	curl -f -L --etag-save "${file}.etag" --etag-compare "${file}.etag" -o "${file}" "https://cdnjs.cloudflare.com/ajax/libs/leaflet/${LEAFLET_VERSION}/${file}"
 done
 
-for file in ${MAPBOX_GL_FILES}; do
-	curl -f -L --etag-save "${file}.etag" --etag-compare "${file}.etag" -o "${file}" "https://api.mapbox.com/mapbox-gl-js/v${MAPBOX_GL_VERSION}/${file}"
+for file in ${MAPLIBRE_GL_FILES}; do
+	curl -f -L --etag-save "${file}.etag" --etag-compare "${file}.etag" -o "${file}" "https://unpkg.com/maplibre-gl@${MAPLIBRE_GL_VERSION}/dist/${file}"
 done
 
-for file in ${MAPBOX_GL_LEAFLET_FILES}; do
-	curl -f -L --etag-save "${file}.etag" --etag-compare "${file}.etag" -o "${file}" "https://cdnjs.cloudflare.com/ajax/libs/mapbox-gl-leaflet/${MAPBOX_GL_LEAFLET_VERSION}/${file}"
+for file in ${MAPLIBRE_GL_LEAFLET_FILES}; do
+	curl -f -L --etag-save "${file}.etag" --etag-compare "${file}.etag" -o "${file}" "https://unpkg.com/@maplibre/maplibre-gl-leaflet@${MAPLIBRE_GL_LEAFLET_VERSION}/${file}"
 done
 
 for file in ${LEAFLET_POLYLINE_FILES}; do
